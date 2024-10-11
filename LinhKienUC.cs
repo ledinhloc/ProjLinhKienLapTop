17336e58f118bf7cb1f6246fbaa471c6bb04dc7f
minhkhoavo <v.minhkhoa123456@gmail.com>
2024-10-11 20:29:55 +0700

    build: view LinhKien by category

---
+++ LinhKienUC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProCuaHangLinhKienLaptop
+{
+    public partial class LinhKienUC : UserControl
+    {
+        public int MaLinhKien { get; set; }
+        public string TenLinhKien { get; set; }
+        public string MoTaChiTiet { get; set; }
+        public decimal GiaBan { get; set; }
+        public decimal GiaNhap { get; set; }
+        public int SoLuongTonKho { get; set; }
+        public LinhKienUC()
+        {
+            InitializeComponent();
+        }
+
+        public LinhKienUC(int maLinhKien, string tenLinhKien, string moTaChiTiet, decimal giaBan, decimal giaNhap, int soLuongTonKho)
+        {
+            InitializeComponent();
+            MaLinhKien = maLinhKien;
+            TenLinhKien = tenLinhKien;
+            MoTaChiTiet = moTaChiTiet;
+            GiaBan = giaBan;
+            GiaNhap = giaNhap;
+            SoLuongTonKho = soLuongTonKho;
+            lblProductName.Text = TenLinhKien;
+            lblProductPrice.Text = GiaBan.ToString() + "VND";
+        }
+        public LinhKienUC(int maLinhKien, string tenLinhKien, decimal giaBan)
+        {
+            InitializeComponent();
+            MaLinhKien = maLinhKien;
+            TenLinhKien = tenLinhKien;
+            GiaBan = giaBan;
+            lblProductName.Text = TenLinhKien;
+            lblProductPrice.Text = GiaBan.ToString() + "VND";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProCuaHangLinhKienLaptop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fTaoDonHang());
         }
     }
 }
