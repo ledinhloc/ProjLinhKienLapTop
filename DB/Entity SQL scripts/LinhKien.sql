4f8fa1b8e19118e2da4b846f97a8a499e77dacdf
nhanth301 <nhanthien.tnn@gmail.com>
2024-10-21 11:53:44 +0700

    Thong Ke DT&TK

--- DB/Entity SQL scripts/LinhKien.sql
+++ DB/Entity SQL scripts/LinhKien.sql
@@ -23,5 +23,94 @@ BEGIN
     SELECT * FROM dbo.LinhKien
     WHERE MaLinhKien = @MaLinhKien;
 END;
+GO
 
+
+CREATE FUNCTION fn_DoanhThuTheoLinhKien(@MaLinhKien INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @DoanhThu INT;
+
+    SELECT @DoanhThu = SUM(ISNULL(SoLuong, 0) * ISNULL(GiaBan, 0))
+    FROM ChiTietDonHang
+    WHERE MaLinhKien = @MaLinhKien;
+
+    RETURN ISNULL(@DoanhThu, 0);
+END;
+GO
+
+CREATE FUNCTION fn_DoanhThuTheoLoaiLinhKien(@MaLoaiLinhKien INT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @DoanhThu INT;
+	SELECT @DoanhThu = SUM(C.SoLuong * C.GiaBan)
+	FROM ChiTietDonHang C
+	INNER JOIN LinhKien L
+	ON C.MaLinhKien = L.MaLinhKien
+	WHERE L.MaLoaiLinhKien = @MaLoaiLinhKien
+	RETURN ISNULL(@DoanhThu, 0);
+END;
+GO
+
+CREATE FUNCTION fn_ThongKeDoanhThuLinhKien()
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT TenLinhKien, dbo.fn_DoanhThuTheoLinhKien(MaLinhKien) AS DoanhThu 
+	FROM LinhKien
+	)
+GO
+
+CREATE FUNCTION fn_ThongKeDoanhThuLoaiLinhKien()
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT TenLoaiLinhKien, dbo.fn_DoanhThuTheoLoaiLinhKien(MaLoaiLinhKien) AS DoanhThu
+	FROM LoaiLinhKien
+)
+
+GO
+
+CREATE FUNCTION fn_HTKTheoLinhKien(@MaLinhKien INT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @SL INT
+	SELECT @SL=SoLuongTonKho
+	FROM LinhKien
+	WHERE MaLinhKien=@MaLinhKien
+	RETURN ISNULL(@SL, 0);
+END
+GO
+
+CREATE FUNCTION fn_HTKTheoLoaiLinhKien(@MaLoaiLinhKien INT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @SL INT
+	SELECT @SL=SUM(SoLuongTonKho)
+	FROM LinhKien
+	WHERE MaLoaiLinhKien = @MaLoaiLinhKien
+	RETURN ISNULL(@SL, 0);
+END
+GO
+
+CREATE FUNCTION fn_ThongKeHTKTheoLinhKien()
+RETURNS TABLE
+RETURN (
+	SELECT TenLinhKien, dbo.fn_HTKTheoLinhKien(MaLinhKien) as SoLuong
+	FROM LinhKien
+)
+GO
+
+CREATE FUNCTION fn_ThongKeHTKTheoLoaiLinhKien()
+RETURNS TABLE
+RETURN(
+	SELECT TenLoaiLinhKien, dbo.fn_HTKTheoLoaiLinhKien(MaLoaiLinhKien) as SoLuong
+	FROM LoaiLinhKien
+)
 
---
+++ Quanly/fThongKeLinhKien.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProCuaHangLinhKienLaptop.DB;
+namespace ProCuaHangLinhKienLaptop.Quanly
+{
+    public partial class fThongKeLinhKien : Form
+    {
+        DataProvider dataProvider = new DataProvider();
+        public fThongKeLinhKien()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void fThongKeLinhKien_Load(object sender, EventArgs e)
+        {
+            string query1 = "SELECT * FROM fn_ThongKeDoanhThuLoaiLinhKien()";
+            string query2 = "SELECT * FROM fn_ThongKeDoanhThuLinhKien()";
+            string query3 = "SELECT * FROM fn_ThongKeHTKTheoLoaiLinhKien()";
+            string query4 = "SELECT * FROM fn_ThongKeHTKTheoLinhKien()";
+            try
+            {
+
+                DataTable linhKienTable1 = dataProvider.ExecuteReader(CommandType.Text, query1);
+                DataTable linhKienTable2 = dataProvider. ExecuteReader(CommandType.Text, query2);
+                DataTable linhKienTable3 = dataProvider.ExecuteReader(CommandType.Text, query3);
+                DataTable linhKienTable4 = dataProvider.ExecuteReader(CommandType.Text, query4);
+                dataGridView1.DataSource = linhKienTable1;
+                dataGridView2.DataSource = linhKienTable2;
+                dataGridView3.DataSource = linhKienTable4;
+                dataGridView4.DataSource = linhKienTable3;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message);
+            }
+        }
+    }
+}
