9b6db4615f455f2027e28f632d08194b6d6e8c76
nhanth301 <nhanthien.tnn@gmail.com>
2024-11-08 01:26:22 +0700

    add tk

--- Quanly/fThongKeLinhKien.cs
+++ Quanly/fThongKeLinhKien.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,12 @@ namespace ProCuaHangLinhKienLaptop.Quanly
             string query2 = "SELECT * FROM fn_ThongKeDoanhThuLinhKien()";
             string query3 = "SELECT * FROM fn_ThongKeHTKTheoLoaiLinhKien()";
             string query4 = "SELECT * FROM fn_ThongKeHTKTheoLinhKien()";
+            string query5 = "sp_ThongTinTopKLinhKien";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                            new SqlParameter("@K", 5),
+                            new SqlParameter("@N", 365)
+            };
             try
             {
 
@@ -36,10 +43,12 @@ namespace ProCuaHangLinhKienLaptop.Quanly
                 DataTable linhKienTable2 = dataProvider. ExecuteReader(CommandType.Text, query2);
                 DataTable linhKienTable3 = dataProvider.ExecuteReader(CommandType.Text, query3);
                 DataTable linhKienTable4 = dataProvider.ExecuteReader(CommandType.Text, query4);
+                DataTable linhKienTable5 = dataProvider.ExecuteReader(CommandType.StoredProcedure, query5, parameters);
                 dataGridView1.DataSource = linhKienTable1;
                 dataGridView2.DataSource = linhKienTable2;
                 dataGridView3.DataSource = linhKienTable4;
                 dataGridView4.DataSource = linhKienTable3;
+                dataGridView5.DataSource = linhKienTable5;
             }
             catch (Exception ex)
             {
@@ -48,6 +57,31 @@ namespace ProCuaHangLinhKienLaptop.Quanly
         }
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView5_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
